bce769fda733c8b1ce3028e0d774f40f52acdc1c
ye <yushunchn@gmail.com>
2019-09-16 19:08:03 +0800

    todo-current location

---
+++ app/src/main/java/com/exampe/LBSdemo/MainActivity.java
@@ -0,0 +1,130 @@
+package com.exampe.LBSdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    LocationClient mLocatioClient;
+    TextView positionText;
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        mLocatioClient = new LocationClient(getApplicationContext());
+        mLocatioClient.registerLocationListener(new MyLocationListener());
+        SDKInitializer.initialize(getApplicationContext());
+
+        setContentView(R.layout.activity_main);
+
+        positionText = (TextView) findViewById(R.id.position_text_view);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if(!permissionList.isEmpty()){
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+        } else {
+            requestLocation();
+        }
+    }
+
+    private void requestLocation() {
+        initLocation();
+        mLocatioClient.start();
+    }
+
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setScanSpan(5000);
+        option.setIsNeedAddress(true);
+        mLocatioClient.setLocOption(option);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(this, "You must grant all the permissions to run this app!", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Some unknown error...", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+                default:
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocatioClient.stop();
+    }
+
+    private class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            StringBuilder currentPosition = new StringBuilder();
+            currentPosition.append("Latitude: ").append(bdLocation.getLatitude()).append("\n");
+            currentPosition.append("Longtitude: ").append(bdLocation.getLongitude()).append("\n");
+            currentPosition.append(("City: ")).append(bdLocation.getCity()).append("\n");
+            currentPosition.append("Area: ").append(bdLocation.getDistrict()).append("\n");
+            currentPosition.append("Street: ").append(bdLocation.getStreet()).append("\n");
+
+            currentPosition.append("Locating way: ");
+            if (bdLocation.getLocType() == bdLocation.TypeGpsLocation) {
+                currentPosition.append("GPS");
+            } else if (bdLocation.getLocType() == bdLocation.TypeNetWorkLocation) {
+                currentPosition.append("Network");
+            }
+
+            positionText.setText(currentPosition);
+
+        }
+    }
+}
